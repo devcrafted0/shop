bf367ebc16fca478edbe30b431f0212325371d1c
devcrafted0 <pubgabcde1@gmail.com>
2026-08-07 09:35:36 +0100

    db.ts added

---
+++ utils/db.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = globalThis as typeof globalThis & {
+  db?: PrismaClient;
+};
+
+export const db =
+  globalForPrisma.db ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.db = db;
+}
